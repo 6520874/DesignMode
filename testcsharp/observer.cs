aadb8c88544403221babc23849c47085b2d34779
DESKTOP-GHUDGOK\65208 <spf>
2022-10-02 01:03:24 +0800

    观察者模式

---
+++ testcsharp/observer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace testcsharp.Properties
+{
+    public abstract class BaseSubject
+    {
+        public event Action CallObservers;
+
+        public void CallAllObservers()
+        {
+            CallObservers?.Invoke();
+        }
+    }
+
+
+    public abstract class BaseObserver
+    {
+        public abstract void OnEnable();
+        public abstract void OnDisable();
+        public abstract void UpdateMyState();
+    }
+
+
+    public class Subject : BaseSubject
+    {
+        private void Start()
+        {
+            changeState();
+
+        }
+
+
+        private void  changeState()
+        {  
+            Console.WriteLine("Player(Subject) : Drink to heal myself.");
+        }
+        
+        
+    }
+    
+    
+    public class Observer : BaseObserver
+    {
+        public BaseSubject subject;
+	
+        public override void OnEnable()
+        {
+            subject.CallObservers += UpdateMyState;
+        }
+	
+        public override void OnDisable()
+        {
+            subject.CallObservers -= UpdateMyState;
+        }
+	
+        public override void UpdateMyState()
+        {
+            Console.WriteLine("Enemy(Observer) : Attack right now!");
+        }
+    }
+    
+    
+    
+
+
+}
